1979d61944e93b6c9a1d4d8a878d3c948436ef46
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2023-05-18 01:51:12 -0300

    day 3, errors in index.tsx

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,10 @@
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+import 'dotenv/config'
+
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
@@ -10,11 +14,17 @@ app.register(cors, {
   origin: true, // todas as urls de front-end podem acessar o back-end
 })
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP server running on port http://localhost:3333')

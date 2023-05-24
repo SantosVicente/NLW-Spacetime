94f3a078654a7b43d99af3a8966c85a00854d54f
SantosVicente <santos.vicente@aluno.ifsp.edu.br>
2023-05-24 11:13:18 -0300

    releases

--- web/next.config.js
+++ web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', '192.168.15.20'],
+    domains: ['avatars.githubusercontent.com', 'YOUR IP ADDRESS'],
   },
 }
 
